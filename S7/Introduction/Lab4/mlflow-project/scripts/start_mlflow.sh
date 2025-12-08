1c47a3938e793e06ff0ab954bf2d0585330cb0e5
Artemii Popovkin <angap4@gmail.com>
2025-12-08 19:47:02 +1000

    truly done

--- S7/Introduction/Lab4/mlflow-project/scripts/start_mlflow.sh
+++ S7/Introduction/Lab4/mlflow-project/scripts/start_mlflow.sh
@@ -5,4 +5,4 @@ echo "Waiting for PostgreSQL to be ready..."
 sleep 5
 
 echo "Starting MLflow server..."
-exec mlflow server --host 0.0.0.0 --port 5000 --backend-store-uri postgresql://mlflow:mlflow@postgres:5432/mlflowdb --default-artifact-root s3://mlflow-artifacts/ --disable-security-middleware
+exec mlflow server --host 0.0.0.0 --port 5000 --backend-store-uri postgresql://mlflow:mlflow@postgres:5432/mlflowdb --artifacts-destination s3://mlflow-artifacts/ --serve-artifacts --disable-security-middleware
